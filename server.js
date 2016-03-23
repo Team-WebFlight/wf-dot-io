008d8ab167e5262f7037d60c859242a1ef6f8263
tbywong <tbywong@gmail.com>
2016-03-22 17:54:58 -0700

    landing 1.0

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('app listening on 3000');
+});
---
+++ static/js/popup.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  $('#feedback-link').click(function() {
+    $('.form').dialog('open');
+  });
+
+  $('.form').dialog({
+    width: 400,
+    autoOpen: false,
+    modal: true,
+    closeOnEscape: true
+  });
+
+  $('#submit').on('click', function() {
+    alert('Thanks for submitting your feedback!');
+    $('.form').dialog('close');
+  });
+
+  $('#close').on('click', function() {
+    $('.form').dialog('close');
+  });
+});
